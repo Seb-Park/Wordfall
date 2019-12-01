59d3e783cd4002010f4967370becfa329d84469a
Sebastian Park <SebPark03@gmail.com>
2019-12-01 18:42:58 -0500

    Add fonts and make GameManager and Tile Scripts.
    Also add current word at top and game states.

---
+++ Wordfall/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState{PLAYERTURN, PHYSICS, WIN, LOSE}
+
+public class GameManager : MonoBehaviour
+{
+    public GameState state;
+    public string currentWord;
+    public bool isSelecting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Wordfall/Assets/Scripts/LetterTile.cs
+++ Wordfall/Assets/Scripts/LetterTile.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LetterTile : MonoBehaviour
 {
+    public string letter;
+    public TextMeshPro textMesh;
+    public GameManager gm;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gm = GameObject.FindWithTag("GameController").GetComponent<GameManager>();
+        textMesh.text = letter;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnMouseDown()
+    {
+        if (gm.state.Equals(GameState.PLAYERTURN))
+        {
+            gm.isSelecting = true;
+        }
     }
 }
