9cd920073a5451f970cfa1d60c0e78e0e5666780
Sebastian Park <SebPark03@gmail.com>
2019-12-01 19:38:29 -0500

    Add physics and update scripts.
    Now can click on tiles and add to word and render a line.

--- Wordfall/Assets/Scripts/GameManager.cs
+++ Wordfall/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public enum GameState{PLAYERTURN, PHYSICS, WIN, LOSE}
 
@@ -9,15 +10,46 @@ public class GameManager : MonoBehaviour
     public GameState state;
     public string currentWord;
     public bool isSelecting;
+
+    public Camera mainCamera;
+
+    public TextMeshProUGUI currentWordText;
+    public LineRenderer line;
+
+    public List<LetterTile> selectedTiles;
+    public List<Vector2> selectedTilePoints;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        line.positionCount = 0;
+        line.positionCount = 1;
+        mainCamera = Camera.main;   
+    }
+
+    public void UpdateWord(){
+        currentWord = "";
+        for (int i = 0; i < selectedTiles.Count; i++)
+        {
+            currentWord = currentWord + selectedTiles[i].letter;
+        }
+        line.positionCount = selectedTilePoints.Count + 1;
+        for (int i = 0; i < selectedTilePoints.Count; i++)
+        {
+            line.SetPosition(i, selectedTilePoints[i]);
+        }
+        line.SetPosition(line.positionCount - 1, mainCamera.ScreenToWorldPoint(Input.mousePosition));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isSelecting)
+        {
+            currentWordText.text = currentWord;
+
+        }
+        line.SetPosition(line.positionCount - 1, mainCamera.ScreenToWorldPoint(Input.mousePosition));
+
     }
 }
--- Wordfall/Assets/Scripts/LetterTile.cs
+++ Wordfall/Assets/Scripts/LetterTile.cs
@@ -8,6 +8,11 @@ public class LetterTile : MonoBehaviour
     public string letter;
     public TextMeshPro textMesh;
     public GameManager gm;
+
+    [SerializeField]
+    int indexInSelection;
+    bool isSelected;
+    bool hasExited;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,11 +26,59 @@ public class LetterTile : MonoBehaviour
         
     }
 
-    public void OnMouseDown()
-    {
+    public void SubtractLetter(){
+        if(isSelected){
+            isSelected = false;
+            for (int i = indexInSelection; i < gm.selectedTiles.Count; i++){
+                gm.selectedTiles.RemoveAt(indexInSelection);
+                gm.selectedTilePoints.RemoveAt(indexInSelection);
+            }
+            gm.UpdateWord();
+        }
+    }
+
+    public void AddLetter(){
         if (gm.state.Equals(GameState.PLAYERTURN))
         {
             gm.isSelecting = true;
         }
+        if (gm.isSelecting && !isSelected)
+        {
+            isSelected = true;
+            indexInSelection = gm.selectedTiles.Count;
+            gm.selectedTiles.Add(this);
+            gm.selectedTilePoints.Add(transform.position);
+            gm.UpdateWord();
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        AddLetter();
+    }
+
+    private void OnMouseOver()
+    {
+        if(gm.isSelecting){
+            if (!isSelected)
+            {
+                AddLetter();
+            }
+        }
+        if(hasExited){
+            if (!isSelected)
+            {
+                AddLetter();
+            }
+            else{
+                SubtractLetter();
+            }
+            hasExited = false;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        hasExited = true;
     }
 }
