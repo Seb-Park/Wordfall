ff304e45927d0023fa09c4f9b7e26919170801b2
Sebastian Park <SebPark03@gmail.com>
2019-12-09 18:35:50 -0500

    Add options panel animation.
    Also fix bug where menu would disappear after exiting options panel.

--- Wordfall/Assets/Scripts/MenuManager.cs
+++ Wordfall/Assets/Scripts/MenuManager.cs
@@ -7,6 +7,9 @@ public class MenuManager : MonoBehaviour
 {
 
     public GameObject FadeInTransition;
+
+    public GameObject MainCanvas;
+    public Animator MainCanvasAnimator;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,21 @@ public class MenuManager : MonoBehaviour
         
     }
 
+    public void WaitBeforeSettingActiveFalse(GameObject item){
+        StartCoroutine(WaitBeforeActive(item, 3f, false));
+    }
+
+    public void OpenMainFromSettings(GameObject mainMenu){
+        mainMenu.SetActive(true);
+        mainMenu.GetComponent<Animator>().SetTrigger("appear");
+    }
+
+    public IEnumerator WaitBeforeActive(GameObject item, float loadTime, bool activityState){
+        yield return new WaitForSeconds(loadTime);
+        Debug.Log("Setting " + item.name + " to active (" + activityState + ")");
+        item.SetActive(activityState);
+    }
+
     public void OpenTimed(){
         FadeInTransition.SetActive(true);
         StartCoroutine(WaitBeforeLoadingScene(1, 1));
@@ -27,5 +45,17 @@ public class MenuManager : MonoBehaviour
     public IEnumerator WaitBeforeLoadingScene(int scene, float loadTime){
         yield return new WaitForSeconds(loadTime);
         SceneManager.LoadScene(scene);
+    }
+
+    public void HideMainCanvas(){
+        MainCanvasAnimator.SetTrigger("disappear");
+        StartCoroutine(WaitBeforeActive(MainCanvas, 2f, false));
+    }
+
+    public void ShowMainCanvas()
+    {
+        MainCanvasAnimator.SetTrigger("appear");
+        MainCanvas.SetActive(true);
+        //StartCoroutine(WaitBeforeActive(MainCanvas, 3f, true));
     }
 }
